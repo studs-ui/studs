de91733c6a00a80853c4a7ca268b76bb87131753
James Zechman <jzechman@strongtie.com>
2023-08-01 15:08:27 -0500

    update svg colors to vars

--- src/components/display/accordian-item.ts
+++ src/components/display/accordian-item.ts
@@ -25,7 +25,7 @@ export class StudsAccordianItem extends LitElement {
           size="medium"
           buttontype="icon"
           icon='<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
-<path d="M11 7.33325L5.5 12.8333L6.7925 14.1258L11 9.92742L15.2075 14.1258L16.5 12.8333L11 7.33325Z" fill="#DF4907"/>
+<path d="M11 7.33325L5.5 12.8333L6.7925 14.1258L11 9.92742L15.2075 14.1258L16.5 12.8333L11 7.33325Z" fill="var(--cta)"/>
 </svg>'
           @click=${this.onToggle}
         ></studs-button>
--- src/components/display/accordian.ts
+++ src/components/display/accordian.ts
@@ -32,7 +32,7 @@ export class StudsAccordian extends LitElement {
             iconposition="end"
             icon='<svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
 <g clip-path="url(#clip0_13874_4085)">
-<rect width="14" height="14" transform="translate(0 0.5)" fill="#DF4907"/>
+<rect width="14" height="14" transform="translate(0 0.5)" fill="var(--cta)"/>
 <path d="M11.0832 8.08341H7.58317V11.5834H6.4165V8.08341H2.9165V6.91675H6.4165V3.41675H7.58317V6.91675H11.0832V8.08341Z" fill="white"/>
 </g>
 <defs>
@@ -48,7 +48,7 @@ export class StudsAccordian extends LitElement {
             @click=${this.onCollapseAll}
             iconposition="end"
             icon='<svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
-<path d="M11.0832 8.08341H7.58317V11.5834H6.4165V8.08341H2.9165V6.91675H6.4165V3.41675H7.58317V6.91675H11.0832V8.08341Z" fill="#DF4907"/>
+<path d="M11.0832 8.08341H7.58317V11.5834H6.4165V8.08341H2.9165V6.91675H6.4165V3.41675H7.58317V6.91675H11.0832V8.08341Z" fill="var(--cta)"/>
 </svg>'
             >Collapse All</studs-button
           >
