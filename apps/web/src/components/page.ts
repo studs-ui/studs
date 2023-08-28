9762ff4ff9f2533bda053de58908a282c025a22b
James Zechman <jzechman@strongtie.com>
2023-08-28 18:02:54 -0500

    Button updates

--- apps/web/src/components/page.ts
+++ apps/web/src/components/page.ts
@@ -36,40 +36,40 @@ export class PresentationalPage extends LitElement {
           <div class="componentGrid">
             <presentational-component><studs-button onclick="alert('clicked')">CTA Button</studs-button></presentational-component>
             <presentational-component>
-              <studs-button buttontype="primary" onclick="alert('clicked')">Primary</studs-button>
+              <studs-button button-type="primary" onclick="alert('clicked')">Primary</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="secondary" onclick="alert('clicked')">Secondary</studs-button>
+              <studs-button button-type="secondary" onclick="alert('clicked')">Secondary</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="tertiary" onclick="alert('clicked')">Tertiary</studs-button>
+              <studs-button button-type="tertiary" onclick="alert('clicked')">Tertiary</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="link" onclick="alert('clicked')">Link</studs-button>
+              <studs-button button-type="link" onclick="alert('clicked')">Link</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="floating" icon="info" onclick="alert('clicked')"></studs-button>
+              <studs-button button-type="floating" icon="info" onclick="alert('clicked')"></studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="icon" icon="info" onclick="alert('clicked')"></studs-button>
+              <studs-button button-type="icon" icon="info" onclick="alert('clicked')"></studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="primary" iconposition="start" icon="info" onclick="alert('clicked')">Icon Left</studs-button>
+              <studs-button button-type="primary" icon-position="start" icon="info" onclick="alert('clicked')">Icon Left</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="primary" iconposition="end" icon="info" onclick="alert('clicked')">Icon Right</studs-button>
+              <studs-button button-type="primary" icon-position="end" icon="info" onclick="alert('clicked')">Icon Right</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="primary" contentdirection="vertical" icon="info" onclick="alert('clicked')">Vertical</studs-button>
+              <studs-button button-type="primary" content-direction="vertical" icon="info" onclick="alert('clicked')">Vertical</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="primary" disabled>Disabled</studs-button>
+              <studs-button button-type="primary" disabled>Disabled</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="primary" size="large">Large</studs-button>
+              <studs-button button-type="primary" size="large">Large</studs-button>
             </presentational-component>
             <presentational-component>
-              <studs-button buttontype="primary" size="small">Small</studs-button>
+              <studs-button button-type="primary" size="small">Small</studs-button>
             </presentational-component>
           </div>
         </div>
