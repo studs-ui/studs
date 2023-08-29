a1421e83e1566c9ab5b21b98ab108026f3ede1e0
Venkata Jillella <vjillella@strongtie.com>
2023-08-29 12:34:43 -0400

    Breadcrumbs first commit

--- apps/web/src/components/page.ts
+++ apps/web/src/components/page.ts
@@ -3259,6 +3259,31 @@ export class PresentationalPage extends LitElement {
           </div>
         </div>
       </div>
+      <button slot="tablist">Breadcrumbs</button>
+      <div slot="Breadcrumbs">
+        <div class="group">
+          <h2>Usage</h2>
+          <presentational-usage></presentational-usage>
+        </div>
+        <div class="group">
+          <div class="componentGrid">
+            <presentational-component>
+              <studs-breadcrumbs>
+                <a href="http://www.strongtie.com">Home</a>
+                <a href="https://www.strongtie.com/about">About Us</a>
+                <a href="https://www.strongtie.com/contact">Contact</a>
+              </studs-breadcrumbs> 
+            </presentational-component>
+            <presentational-component>
+              <studs-breadcrumbs separator=">">
+                <a href="http://www.strongtie.com">Home</a>
+                <a href="https://www.strongtie.com/about">About Us</a>
+                <a href="https://www.strongtie.com/contact">Contact</a>
+              </studs-breadcrumbs> 
+            </presentational-component>
+          </div>
+        </div>
+      </div>
     </presentational-tabs>
     `;
   }
