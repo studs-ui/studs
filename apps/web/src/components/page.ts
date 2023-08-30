5a4dbed1d4ff7cd59d3d34b6237af23a21cd1035
JP Pohlmann <jpohlmann@strongtie.com>
2023-08-29 18:10:24 -0700

    [STUDS-274]  base styles, placeholder mixin, themes update

--- apps/web/src/components/page.ts
+++ apps/web/src/components/page.ts
@@ -824,27 +824,27 @@ return html`
     <div class="group">
       <div class="componentGrid">
         <presentational-component>
-          <studs-input type="text" variant="outlined" inputSize="large" label="My Label" placeholder="Enter your name" adornment="lbs" adornment-position="start">
+          <studs-input type="text" inputSize="large" label="My Label" placeholder="Enter your name" adornment="lbs" adornment-position="start">
           </studs-input>
         </presentational-component>
         <presentational-component>
-          <studs-input autocomplete="off" name="fname" type="text" value="John" label="Adornment End" variant="outlined" input-size="normal" adornment="lbs" adornment-position="end" @change="${(e: CustomEvent) =>
+          <studs-input autocomplete="off" name="fname" type="text" value="John" label="Adornment End" input-size="normal" adornment="lbs" adornment-position="end" @change="${(e: CustomEvent) =>
                 console.log('Selected value:', e.detail)}"></studs-input>
         </presentational-component>
         <presentational-component>
-          <studs-input type="email" name="email" label="Email" variant="outlined" @change="${(e: CustomEvent) =>
+          <studs-input type="email" name="email" label="Email" @change="${(e: CustomEvent) =>
                 console.log('Selected value:', e.detail)}"></studs-input>
         </presentational-component>
         <presentational-component>
-          <studs-input type="search" name="search" label="Search" variant="outlined" @change="${(e: CustomEvent) =>
+          <studs-input type="search" name="search" label="Search" @change="${(e: CustomEvent) =>
                 console.log('Selected value:', e.detail)}"></studs-input>
         </presentational-component>
         <presentational-component>
-          <studs-input type="number" name="number" label="Number" variant="outlined" @change="${(e: CustomEvent) =>
+          <studs-input type="number" name="number" label="Number" @change="${(e: CustomEvent) =>
                 console.log('Selected value:', e.detail)}"></studs-input>
         </presentational-component>
         <presentational-component>
-          <studs-input type="password" name="password" label="Password" variant="outlined" @change="${(e: CustomEvent) =>
+          <studs-input type="password" name="password" label="Password" @change="${(e: CustomEvent) =>
                 console.log('Selected value:', e.detail)}"></studs-input>
         </presentational-component>
       </div>
