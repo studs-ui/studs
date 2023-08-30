9ae510a8322a814cfd1d7266c648df2ebbf04439
James Zechman <jzechman@strongtie.com>
2023-08-30 15:35:31 -0500

    [STUDS-365] Popover Adjustments

--- packages/ui/src/components/overlays/popover.ts
+++ packages/ui/src/components/overlays/popover.ts
@@ -1,12 +1,12 @@
 import style from '@studs/styles/components/popover.scss?inline';
-import { LitElement, TemplateResult, html, unsafeCSS } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { LitElement, PropertyValueMap, html, nothing, unsafeCSS } from 'lit';
+import { customElement, queryAssignedElements, state } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { PopperController } from '../../controllers/popperController';
 import { WithPopper, WithPopperInterface } from '../../mixins/withPopper';
 
 export interface PopoverProps extends WithPopperInterface {
-  children?: TemplateResult | HTMLElement | string;
+  children?: string | null | undefined;
 }
 
 @customElement('studs-popover')
@@ -15,25 +15,51 @@ export class StudsPopover extends WithPopper(LitElement) {
     super();
     if (this.popperController) this.popperController.on = 'click';
   }
+
   static styles = [unsafeCSS(style), PopperController.styles];
+
+  @queryAssignedElements({ slot: 'footer' }) _footer!: HTMLElement[];
+  @state() isFooter: boolean = false;
+
+  protected firstUpdated(
+    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>
+  ): void {
+    super.firstUpdated(_changedProperties);
+    if (this._footer.length > 0) {
+      this.isFooter = true;
+      this.scheduleUpdate();
+    }
+  }
 
   render() {
     return html`<div
       class=${classMap({
         popper: true,
         popover: true,
+        '-wrapper': true,
         [`-${this.position}`]: true,
       })}
       role="popover"
     >
-      <slot></slot>
-      <studs-button
-        aria-label="Close popover"
-        button-type="close"
-        size="small"
-        icon="close"
-        @click=${this.popperController?.hidePopper}
-      ></studs-button>
+      <header class="popover -title">
+        <slot name="title"></slot>
+        <studs-button
+          aria-label="Close popover"
+          button-type="close"
+          size="small"
+          icon="close"
+          @click=${this.popperController?.hidePopper}
+        ></studs-button>
+      </header>
+      <slot class="popover -media" name="media"></slot>
+      <main><slot></slot></main>
+      <!-- Renders Footer Container if Footer content is present
+           Otherwise, returns slot for addition of content -->
+      ${this.isFooter
+        ? html`<footer class="popover -footer">
+            <slot name="footer"></slot>
+          </footer>`
+        : html`<slot name="footer"></slot>`}
       <div id="arrow"></div>
     </div>`;
   }
--- packages/ui/src/stories/overlays/Popover.stories.ts
+++ packages/ui/src/stories/overlays/Popover.stories.ts
@@ -3,13 +3,14 @@ import { html } from 'lit';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { PopoverProps } from '../../components/overlays/popover';
 import { position } from '../../utils/_argTypes';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 
 const meta = {
   title: 'Studs/Overlays/Popover',
   tags: ['autodocs'],
   render: (args) => html`<studs-button
     >Popover<studs-popover position=${ifDefined(args.position)}>
-      ${args.children}
+      ${unsafeHTML(args.children)}
     </studs-popover></studs-button
   >`,
   argTypes: {
@@ -23,6 +24,19 @@ type Story = StoryObj<PopoverProps>;
 export const Default: Story = {
   args: {
     position: 'bottom',
-    children: 'This is a popover',
+    children: `
+    <p slot="title">This is a popover</p>
+    <studs-image src="https://ssttoolbox.widen.net/content/cusbzivu9t/jpeg/C_SCLC_InstPho_RI_Inst_StairCassetteLedgerConnector-exploded_C0.jpg?crop=true&keep=c&q=60&w=900&h=675" slot="media"></studs-image>
+    <main>Popover content</main>
+    <studs-button button-type="primary" size="small" slot="footer">Yes</studs-button>`,
+  },
+};
+
+export const Basic: Story = {
+  args: {
+    position: 'right',
+    children: `
+    <p slot="title">This is a popover</p>
+    <main>Popover content</main>`,
   },
 };
