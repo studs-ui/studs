05b740880389dce912dd1f7b310e3c175516d6ec
James Zechman <jzechman@strongtie.com>
2023-09-01 15:26:49 -0500

    Addition of Docusarus Test Site

---
+++ apps/docs-dc/src/global.ts
@@ -0,0 +1,6 @@
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import 'node_modules/@studs/wc';
+
+if (ExecutionEnvironment.canUseDOM) {
+  // As soon as the site loads in the browser, register a global event listener
+}
