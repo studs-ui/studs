686b9b305e3e1b4d46aa07b09a61c63cce74f6a6
James Zechman <jzechman@strongtie.com>
2023-09-05 14:15:30 -0500

    Add DTS

--- apps/docs/src/components/HomepageFeatures/index.tsx
+++ apps/docs/src/components/HomepageFeatures/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+
 type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
