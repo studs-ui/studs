dda01927345ed2bd625ba68903687756e8624f2a
Nghia Nguyen <nnnghia98@gmail.com>
2023-09-14 08:54:27 +0700

    resolve conflicts

---
+++ apps/docs/src/utils/constants.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const FORM_PROPS = [
+  {
+    name: 'name',
+    description: <></>,
+    reflects: false,
+    type: 'string',
+  },
+  {
+    name: 'placeholder',
+    description: <></>,
+    reflects: false,
+    type: 'string',
+  },
+];
