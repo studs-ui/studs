c2eb445d450f9a34ed599f871b93c5ffcdc4e953
James Zechman <jzechman@strongtie.com>
2023-10-02 15:27:26 -0500

    [STUDS-481] Add Accessability Roles

--- packages/ui/src/components/display/menu.ts
+++ packages/ui/src/components/display/menu.ts
@@ -21,10 +21,19 @@ export class StudsMenu extends WithPopper(LitElement) {
         <div class="menu -wrapper">
             <slot name="trigger"></slot>
             <div role="menu" class="popper">
-                <slot></slot>
+                <slot @slotchange=${this.onSlotChange}></slot>
                 <div id="arrow"></div>
             </div>
         </div>
         `
+    }
+
+    private onSlotChange(e) {
+        const target = e.target as HTMLSlotElement;
+        const assigned = target.assignedElements();
+        assigned.forEach((el) => {
+            el.setAttribute('role', 'menuitem');
+        }
+        );
     }
 } 
