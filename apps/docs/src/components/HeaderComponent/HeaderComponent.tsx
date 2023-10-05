62799c240b79bdb738773d2d466d0a53a5d9eeb3
James Zechman <jzechman@strongtie.com>
2023-10-05 13:40:22 -0500

    [STUDS-445] Ensure Studs Components Design and Usage

--- apps/docs/src/components/HeaderComponent/HeaderComponent.tsx
+++ apps/docs/src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,29 +1,15 @@
-import React, { useEffect, useRef } from 'react';
-import styles from './styles.module.scss';
-import useOnScreen from '@site/src/hooks/use0nScreen';
-import { useDocsVersion } from '@docusaurus/theme-common/internal';
-import Link from '@docusaurus/Link';
-import { BASE_URL_GITHUB, BASE_URL_STRB, MAIN_GITHUB_URL } from '@site/src/utils/constants';
 import BrowserOnly from '@docusaurus/BrowserOnly';
+import Link from '@docusaurus/Link';
+import { useDocsVersion } from '@docusaurus/theme-common/internal';
+import { BASE_URL_GITHUB, BASE_URL_STRB, MAIN_GITHUB_URL } from '@site/src/utils/constants';
+import React from 'react';
+import styles from './styles.module.scss';
 
 const HeaderComponent = ({ htmlTag, jsxTag, urlGithub, urlStrbook, status }) => {
   const { label: version, banner, badge, ...rest } = useDocsVersion();
 
 
   const tag = `<${htmlTag}>`;
-  const ref = useRef<HTMLDivElement>(null);
-  const isVisible = useOnScreen(ref);
-
-  // useEffect(() => {
-  //   const query = document.querySelectorAll('.custom');
-  //   query.forEach((item) => {
-  //     const versionElement = item.shadowRoot.querySelector('.-infor');
-  //     versionElement?.setAttribute(
-  //       'style',
-  //       'background-color: #444444; color: #fff; border: 1px solid transparent; border-radius: 1rem; padding: 0.25rem 0.5rem'
-  //     );
-  //   });
-  // }, [isVisible]);
   return (
     <div className={styles.header}>
       <code>
@@ -38,7 +24,6 @@ const HeaderComponent = ({ htmlTag, jsxTag, urlGithub, urlStrbook, status }) => 
           const StudsChip =
             require('@studs/react').StudsChip;
           return <StudsChip
-          ref={ref}
           className={`custom ${styles.custom} ${styles[status.toLowerCase()]}`}
           size="small"
           selected
@@ -47,33 +32,24 @@ const HeaderComponent = ({ htmlTag, jsxTag, urlGithub, urlStrbook, status }) => 
         </StudsChip>;
         }}
         </BrowserOnly>
-        {/* <studs-chip
-          ref={ref}
-          class={`custom ${styles.custom} ${styles[status.toLowerCase()]}`}
-          size="small"
-          selected
-        >
-          {status}
-        </studs-chip> */}
         <Link
           to={urlGithub ? `${BASE_URL_GITHUB}/${urlGithub}`: MAIN_GITHUB_URL}
           target="_blank"
-          className={styles.wrapper}
         >
-          <img src="/img/github.svg" alt="github" className={styles.img} />
-          <span>Github</span>
+          <studs-button button-type='link' size="small"><img src="/img/github.svg" alt="github" className={styles.img} /> Github</studs-button>
         </Link>
         <Link
-          className={`${styles.custom} ${styles.wrapper}`}
           to={`${BASE_URL_STRB}/${urlStrbook}`}
           target="_blank"
         >
+          <studs-button button-type='link' size="small">
           <img
             src="/img/storybook.svg"
             alt="storybook"
             className={styles.img}
           />
-          <span>Storybook</span>
+          Storybook
+          </studs-button>
         </Link>
       </div>
     </div>
